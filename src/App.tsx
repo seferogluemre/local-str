48e7f44f2c5e815c0c335692ae23ac86665ea0b0
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-29 00:02:08 +0300

    data push storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 
-import { Container, Form, FormControl } from 'react-bootstrap'
+import { Button, Container, Form, FormControl, Row } from 'react-bootstrap'
 import './App.css'
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface User {
   firstName: string;
@@ -10,33 +11,45 @@ interface User {
 
 }
 
-
 function App() {
   const [user, setUsers] = useState<User[]>([]);
-  const [firstName, setFirstName] = useState<string>("");
-  const [lastName, setLastName] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
+  const [firstNameValue, setFirstName] = useState<string>("");
+  const [lastNameValue, setLastName] = useState<string>("");
+  const [ageValue, setAge] = useState<number>(0);
 
 
   const handleDataForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!firstNameValue) return;
+    if (!lastNameValue) return;
+
+    const newUser: User = {
+      firstName: firstNameValue.toLocaleLowerCase(),
+      lastName: lastNameValue.toLocaleLowerCase(),
+      age: +ageValue,
+    }
+
+    setUsers([...user, newUser]);
+    alert("Kullanıcı sisteme eklendi");
+    localStorage.setItem("users", JSON.stringify(user))
   }
 
 
   return (
     <>
-
       <Container>
         <Form onSubmit={handleDataForm}>
           <div className='my-2'>
-            <FormControl type='text' placeholder='İsminizi giriniz...' value={firstName} onChange={(e) => setFirstName(e.target.value)} className='w-25' />
+            <FormControl type='text' placeholder='İsminizi giriniz...' value={firstNameValue} onChange={(e) => setFirstName(e.target.value)} className=' form-control' />
           </div>
           <div className='my-2'>
-            <FormControl type='text' placeholder='İsminizi giriniz...' value={lastName} onChange={(e) => setLastName(e.target.value)} className='w-25' />
+            <FormControl type='text' placeholder='Soyisminizi giriniz...' value={lastNameValue} onChange={(e) => setLastName(e.target.value)} className=' form-control' />
           </div>
           <div className='my-2'>
-            <FormControl type='number' placeholder='İsminizi giriniz...' value={age} onChange={(e) => setAge(+e.target.value)} className='w-25' />
+            <FormControl type='number' placeholder='Yaşınızı giriniz...' value={ageValue} onChange={(e) => setAge(+e.target.value)} className=' form-control' />
           </div>
+          <Button type='submit'>Kaydol</Button>
         </Form>
       </Container>
 
